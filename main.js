3cfc44c592416ef975aa135b56c3a136147704d0
Elisa Guadalupe Contreras Ocampo <148170599+elisacooc@users.noreply.github.com>
2023-10-25 21:55:00 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Soy aprendíz de programación web y hago contenido de tecnología.</span>')
+  .typeString('<span style="color: #fb4993;">Soy aprendíz de programación web en el Bootcamp de TecnolochicasPro.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
